1d90e5679814e0d5350d155c0c2142453a28e820
huzaifa khan <hk4899541@gmail.com>
2024-05-18 13:40:03 +0500

    all files uploaded

---
+++ index.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+
+const res = await inquirer.prompt([
+  {
+    name: "userinput",
+    type: "input",
+    message: "Please enter the number of seconds:",
+    validate: (input) => {
+      if (isNaN(input)) {
+        return "Please enter a valid number";
+      } else if (input > 60) {
+        return "Seconds must be in 60";
+      }
+      else{
+        return true;
+      }
+    },
+  },
+]);
+
+const input = parseInt(res.userinput);
+starttime(input);
+
+function starttime(val: number) {
+  const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+  const intervalTime = new Date(intTime);
+  setInterval(() => {
+    const currentTime = new Date();
+    const timeDiff = differenceInSeconds(intervalTime, currentTime);
+    if (timeDiff <= 0) {
+      console.log("Timer has expired");
+      process.exit(0);
+    }
+    const minute = Math.floor((timeDiff % (3600 / 24)) / 3600);
+    const seconds = Math.floor(timeDiff % 60);
+    console.log(
+      `${minute.toString().padStart(2, "0")} : ${seconds
+        .toString()
+        .padStart(2, "0")}`
+    );
+  }, 1000);
+}
